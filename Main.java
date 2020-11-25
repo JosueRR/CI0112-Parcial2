eb0deafca746c6d105e96527039ae18191aa7eef
Josué <josue-retana@hotmail.com>
2020-11-25 09:50:40 -0600

    V1.1

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+/**
+ * Clase Main: clase principal del proyecto.
+ * 
+ * @author Josué Retana Rodríguez - C06440 
+ * @version 1
+ */
+
+public class Main {
+    public static void main (String args[]) {
+    }
+}
