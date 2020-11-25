d383e3c11ca48feea4d18cba3f4d24440bb9f937
Josué <josue-retana@hotmail.com>
2020-11-25 15:21:19 -0600

    Orden Alfabetico Mejorado

--- ListaEstudiante.java
+++ ListaEstudiante.java
@@ -33,36 +33,44 @@ public class ListaEstudiante {
      */
     public void agregarEstudiante(String nombre, int carnet, ListaNotas notas, float promedio) {
         Estudiante nuevoNodo = new Estudiante();
+        Estudiante auxiliar = inicio;
         nuevoNodo.setNombre(nombre);
         nuevoNodo.setCarnet(carnet);
         nuevoNodo.setNotas(notas);
         nuevoNodo.setPromedio(promedio);
-        
+
         if(esVacia()) {
             inicio = nuevoNodo;
         }
         else {
-            Estudiante auxiliar = inicio;
-            
-            while( "palo".compareTo("papa") < 0 ) {
-                auxiliar = auxiliar.getSiguiente();
-                
+            if (nuevoNodo.getNombre().compareTo(auxiliar.getNombre()) < 0 ) {
+                nuevoNodo.setSiguiente(auxiliar.getSiguiente());
+                auxiliar.setSiguiente(nuevoNodo);
             }
+            else {
+                while(auxiliar.getSiguiente() != null) {
+                    if (nuevoNodo.getNombre().compareTo(auxiliar.getNombre()) < 0 ) {
+                        nuevoNodo.setSiguiente(auxiliar.getSiguiente());
+                        auxiliar.setSiguiente(nuevoNodo);
+                        break;
+                    }
+                    auxiliar = auxiliar.getSiguiente();
+                }
 
-            auxiliar.setSiguiente(nuevoNodo);
+                nuevoNodo.setSiguiente(auxiliar.getSiguiente());
+                auxiliar.setSiguiente(nuevoNodo);
+            }
         }
         tamanio++;
     }
-    
-    //auxiliar.getNombre()).compareTo(auxiliar.getSiguiente().getNombre()
-    
+
     /**
      * Método toString, convierte la lista en un String
      */
     public String toString(ListaEstudiante listaEstudiantes) {
         String listaEstudiantesString = "";
         Estudiante auxiliar = inicio;
-        
+
         for (int posicion = 0; posicion < listaEstudiantes.getTamanio() ; posicion++) {
             listaEstudiantesString += "Estudiante: " + auxiliar.getNombre() + "\n";
             listaEstudiantesString += "Carnet: " + auxiliar.getCarnet() + "\n";
@@ -70,7 +78,7 @@ public class ListaEstudiante {
             listaEstudiantesString += "--------------------------" + "\n" ;
             auxiliar = auxiliar.getSiguiente();
         }
-        
+
         return listaEstudiantesString;
     }
 }
